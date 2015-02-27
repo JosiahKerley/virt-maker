d3a7faaddfc446f388b48fb793115a8cac600ac3
Josiah Kerley <josiahkerley@gmail.com>
2015-02-27 15:11:12 -0700

    added password directive

---
+++ providers/rootpass.py
@@ -0,0 +1,7 @@
+import os
+def provider(body,hash,args,verbose,image):
+	if verbose:
+		cmd = 'virt-customize ---root-password %s -a %s'%(args,hash)
+	else:
+		cmd = 'virt-customize -q --root-password %s -a %s'%(args,hash)
+	return(os.system(cmd))
