94741b0605c3a3a6c0b93b0a9eb2d4f87a777498
Josiah Kerley <josiahkerley@gmail.com>
2015-02-27 02:30:29 -0700

    aded install

---
+++ providers/create.py
@@ -0,0 +1,27 @@
+defaults = {
+	"connect":"qemu:///system",
+	"virt-type":"kvm",
+	"ram":"500",
+	"disk path":"<[args]>,size=10",
+	"network":"network=default",
+	"graphics":"vnc",
+	"os-variant":"generic",
+}
+
+import os
+def provider(body,hash,args,verbose,image):
+	for line in body.split('\n'):
+		if '=' in line:
+			left = line.split('=')[0]
+			right = line.replace(left,'')
+			defaults[left] = right
+	cmdargs = ' '
+	for i in defaults:
+		cmdargs += ' --%s %s'%(i,defaults[i])
+	cmdargs = cmdargs.replace('<[args]>',os.path.abspath(args))
+	if verbose:
+		cmd = 'virt-install %s'%(cmdargs)
+	else:
+		cmd = 'virt-install -q %s'%(cmdargs)
+	print cmd
+	return(os.system(cmd))
