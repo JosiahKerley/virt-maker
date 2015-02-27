33fa34e48aa693432be03b3d2c79e3c624b9a836
Josiah Kerley <josiahkerley@gmail.com>
2015-02-27 01:59:49 -0700

    added verbose to export

--- providers/export.py
+++ providers/export.py
@@ -3,5 +3,8 @@ def provider(body,hash,args,verbose,image):
         retval = False
         if args == "": args = False
         if args and not args == 'export':
-                cmd = 'qemu-img convert -f qcow2 -O qcow2 %s "%s"'%(hash,args)
+				if verbose:
+					cmd = 'qemu-img convert -p -f qcow2 -O qcow2 %s "%s"'%(hash,args)
+				else:
+					cmd = 'qemu-img convert -f qcow2 -O qcow2 %s "%s"'%(hash,args)
                 return(os.system(cmd))
