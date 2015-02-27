c0f4e34d770671a5532c9dd097b417d93adb146c
Josiah Kerley <josiahkerley@gmail.com>
2015-02-27 01:57:44 -0700

    fixed export

--- providers/export.py
+++ providers/export.py
@@ -3,6 +3,5 @@ def provider(body,hash,args,verbose,image):
         retval = False
         if args == "": args = False
         if args and not args == 'export':
-                lastsnap = (current.split('/')[-1]).replace('_mount','')
                 cmd = 'qemu-img convert -f qcow2 -O qcow2 %s "%s"'%(hash,args)
                 return(os.system(cmd))
