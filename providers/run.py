78c46e6f64fe7f220ed9e400a10f498053dcd610
Josiah Kerley <josiahkerley@gmail.com>
2015-02-27 16:04:05 -0700

    added run

---
+++ providers/run.py
@@ -0,0 +1,7 @@
+import os
+def provider(body,hash,args,verbose,image):
+	if verbose:
+		cmd = 'virt-customize --run-command %s -a %s'%(args,hash)
+	else:
+		cmd = 'virt-customize -q --run-command %s -a %s'%(args,hash)
+	return(os.system(cmd))
