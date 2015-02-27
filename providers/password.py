4156e3802cbe4afd83a2a54df9fa561c335e311f
Josiah Kerley <josiahkerley@gmail.com>
2015-02-27 15:13:57 -0700

    trying generic password

---
+++ providers/password.py
@@ -0,0 +1,7 @@
+import os
+def provider(body,hash,args,verbose,image):
+	if verbose:
+		cmd = 'virt-customize ---password %s -a %s'%(args,hash)
+	else:
+		cmd = 'virt-customize -q --password %s -a %s'%(args,hash)
+	return(os.system(cmd))
--- providers/rootpass.py
+++
@@ -1,7 +0,0 @@
-import os
-def provider(body,hash,args,verbose,image):
-	if verbose:
-		cmd = 'virt-customize ---root-password %s -a %s'%(args,hash)
-	else:
-		cmd = 'virt-customize -q --root-password %s -a %s'%(args,hash)
-	return(os.system(cmd))
