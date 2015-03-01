1fcf63e73e3fb3e65e5f15f318bdda042a5ff7eb
Josiah Kerley <josiahkerley@gmail.com>
2015-03-01 01:17:54 -0700

    added cmd

--- providers/qemu-img.py
+++ providers/qemu-img.py
@@ -3,6 +3,7 @@ def provider(body,hash,args,verbose,image):
 	cmd = 'qemu-img'
 	if verbose:
 		cmd = '%s %s'%(cmd,args)
+		print(cmd)
 	else:
 		cmd = '%s %s >/dev/null'%(cmd,args)
 	return(os.system(cmd))
--- providers/virt-customize.py
+++ providers/virt-customize.py
@@ -3,6 +3,7 @@ def provider(body,hash,args,verbose,image):
 	cmd = 'virt-customize'
 	if verbose:
 		cmd = '%s %s'%(cmd,args)
+		print(cmd)
 	else:
 		cmd = '%s --quite %s'%(cmd,args)
 	return(os.system(cmd))
