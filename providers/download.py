6b6ded624c9545636a018585799e0c972d2e2b6a
Josiah Kerley <josiahkerley@gmail.com>
2015-03-02 21:20:18 -0700

    ``

---
+++ providers/download.py
@@ -0,0 +1,15 @@
+import os
+import urllib2
+def provider(body,hash,args,verbose,image,settings):
+	filename = args.split('/')[-1]
+	f = urllib2.urlopen(args)
+	remotesize = f.headers["Content-Length"]
+	if os.path.isfile(filename) and not os.path.getsize(filename) == remotesize:
+		if verbose: print('\tDownloading "%s"'%(args))
+		CHUNK = 16 * 1024
+		with open(filename, 'wb') as fp:
+			while True:
+				chunk = req.read(CHUNK)
+				if not chunk: break
+				fp.write(chunk)
+		if verbose: print('\t"%s" complete'%(filename))
