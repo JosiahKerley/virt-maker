4c99302405b3b05bcc65fad6c159cfe7f4ad8b0e
root <root@khs-jenkins00.khs.org>
2015-03-19 05:40:33 +0000

    More studio work

--- other/virt-maker-studio/virt-maker-studio.py
+++ other/virt-maker-studio/virt-maker-studio.py
@@ -3,6 +3,7 @@ import json
 import redis
 import time
 import shutil
+#import bcrypt
 from gitexpect import Git
 from urlparse import urlparse
 from flask import Flask, request, render_template, render_template_string
@@ -10,8 +11,15 @@ from flask import Flask, request, render_template, render_template_string
 r = redis.Redis()
 
 ## Settings
-fingerprint = 'virt-maker-studio:'
+settings = {
+	'fingerprint':'virt-maker-studio:',
+	'workspace':'/var/lib/virt-maker/studio',
+}
 
+## Startup
+if not os.path.isdir(settings['workspace']): os.makedirs(settings['workspace'])
+os.chdir(settings['workspace'])
+os.system('ls')
 
 ## HTML and CSS
 
@@ -120,6 +128,54 @@ newrepomarkup = '''
 '''
 
 
+## Returns structure of path for treeview
+def pathTree(path,id=0):
+	id += 1
+        d = {'value': os.path.basename(path)}
+        d['path'] = path
+	d['id'] = id
+        if os.path.isdir(path):
+                d['type'] = "folder"
+                d['data'] = [pathTree(os.path.join(path,x),id) for x in os.listdir(path)]
+        else:
+                d['type'] = "file"
+        return(d)
+
+
+
+
+## Gets list of path files/folders
+def listfiles(folder):
+	tree = []
+	for root, folders, files in os.walk(folder):
+		for filename in folders + files:
+			#yield os.path.join(root, filename)
+			tree.append(os.path.join(root, filename))
+	return(tree)
+
+
+## handles repo indexing
+class Repo:
+	creddir = 'repo-credentials'
+	gitdir = 'repos'
+	def __init__(self):
+		if not os.path.isdir(self.creddir): os.makedirs(self.creddir)
+		if not os.path.isdir('%s/%s'%(settings['workspace'],self.gitdir)): os.makedirs('%s/%s'%(settings['workspace'],self.gitdir))
+
+	def save(self,repo):
+		with open('%s/%s.repo'%(self.creddir,repo['name']),'w') as f: f.write(json.dumps(repo,indent=2))
+
+	def repopath(self,name):
+		full = '%s/%s/%s'%(settings['workspace'],self.gitdir,name)
+		if not os.path.isdir(full): return(False)
+		return(full)
+
+	def repofiles(self,name):
+		if self.repopath(name):
+			return(pathTree(self.repopath(name)))
+		else:
+			return(False)
+
 
 
 ##-> MAIN APP <-##
@@ -129,14 +185,14 @@ app = Flask(__name__)
 ## Main page
 @app.route('/', methods=['GET'])
 def mainpage():
-	id = '%s*'%(fingerprint)
+	id = '%s*'%(settings['fingerprint'])
 	items = r.keys(id)
 	items = list(filter(None, items))
 	new = []
 	for i in items:
 		items = list(set(json.loads(r.get(i))))
 		if len(items) > 1:
-			new.append(i.replace(fingerprint,''))
+			new.append(i.replace(settings['fingerprint'],''))
 		else:
 			r.delete(i)
 	items = new
@@ -148,18 +204,22 @@ def mainpage():
 ## Add repo page
 @app.route('/repos/new', methods=['GET','POST'])
 def newpage():
-	id = '%srepos*'%(fingerprint)
+	id = '%srepos*'%(settings['fingerprint'])
 	css = render_template_string(defaultcss,fontstyle=fonts[fontselected]['fontstyle'])
 	if request.method == 'POST':
 		git = Git()
+		repo = Repo()
 		input = dict(request.form)
-		print request.form
-		print json.dumps(input,indent=2)
-		git.url = input['url'][0]
-		git.name = input['name'][0]
-		git.branch = input['branch'][0]
+		#print request.form
+		#print json.dumps(input,indent=2)
+		git.url =      input['url'][0]
+		git.name =     input['name'][0]
+		git.branch =   input['branch'][0]
 		git.username = input['username'][0]
 		git.password = input['password'][0]
+		git.clone('%s/%s'%(repo.gitdir,git.name),True)
+		repo.save({"name":git.name,"branch":git.branch,"username":git.username,"password":git.password})
+		del git
 		html = 'done'
 	else:
 		items = r.keys(id)
@@ -168,12 +228,45 @@ def newpage():
 		for i in items:
 			items = list(set(json.loads(r.get(i))))
 			if len(items) > 1:
-				new.append(i.replace(fingerprint,''))
+				new.append(i.replace(settings['fingerprint'],''))
 			else:
 				r.delete(i)
 		items = new
 		html = render_template_string(newrepomarkup,items=items,link=fonts[fontselected]['link'],css=css)
 	return(html)
+
+
+
+## View single repo
+@app.route('/repos/<name>', methods=['GET','POST'])
+def singlerepo(name):
+	git = Git()
+	repo = Repo()
+	id = '%srepos*'%(settings['fingerprint'])
+	css = render_template_string(defaultcss,fontstyle=fonts[fontselected]['fontstyle'])
+	if True:
+		rpath = repo.repopath(name)
+		if rpath and os.path.isdir(rpath):
+			tree = repo.repofiles(name)
+			tree = {"type": "folder","value": "sdf", "data":[{"value":"file"}]}
+			html = json.dumps([tree],indent=2)
+		else:
+			html = 'repo not installed'
+	return(html)
+
+
+
+#@app.route('/repos/new', methods=['GET','POST'])
+
+
+
+
+
+
+
+
+
+
 
 
 
