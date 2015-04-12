d6f8456d806c27d36287dca03d3c305bb19c9953
Josiah Kerley <josiahkerley@gmail.com>
2015-04-11 22:59:22 -0600

    debugging hash build

--- virt-maker.py
+++ virt-maker.py
@@ -69,6 +69,8 @@ def dsl2dict(text,options=False,mutatestr='<[%s]>', providerchar='@'):
 		if sections[-1]['provider'] == '':
 			sections[-1]['provider'] = lastprovider
 		lastprovider = sections[-1]['provider']
+		print lasthash
+		print json.dumps(sections[-1])
 		sections[-1]['hash'] = hashlib.md5(lasthash+json.dumps(sections[-1])).hexdigest()
 		lasthash = sections[-1]['hash']
 	sections.remove(sections[0]) ## Remove blank entry
