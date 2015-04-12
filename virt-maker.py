1017ff58d115d4653d82cda05ed936deead0c859
Josiah Kerley <josiahkerley@gmail.com>
2015-04-12 02:05:44 -0600

    ``

--- virt-maker.py
+++ virt-maker.py
@@ -65,7 +65,7 @@ def dsl2dict(text,options=False,mutatestr='<[%s]>', providerchar='@'):
 		print s
 		with open('/tmp/vm.log','a') as f: f.write(body)
 		#if not (body.split('\n#%s'%(providerchar))[0]).startswith('#@'):
-		if True:
+		if not s.startswith('#'):
 			sections.append(
 				{
 					"provider":head.split(' ')[0],
