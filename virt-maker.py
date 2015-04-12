d36c8c986c90f83680a634815b2af53a9f6a4524
Josiah Kerley <josiahkerley@gmail.com>
2015-04-11 20:54:53 -0600

    Fixed newline body issue

--- virt-maker.py
+++ virt-maker.py
@@ -46,7 +46,7 @@ def dsl2opt(text,providerchar='@'):
 
 ## Parses DSL Statements
 def dsl2dict(text,options=False,mutatestr='<[%s]>', providerchar='@'):
-	text = '\n'+text
+	text = '\n'+text.replace('\n@','\n\n@')
 	if options:
 		for i in options:
 			key = mutatestr%(i.split('=')[0])
