e7288e2d159ab69ede9557b5320560db5890668c
Josiah Kerley <josiahkerley@gmail.com>
2015-04-11 22:50:11 -0600

    ``

--- virt-maker.py
+++ virt-maker.py
@@ -66,7 +66,7 @@ def dsl2dict(text,options=False,mutatestr='<[%s]>', providerchar='@'):
 				"body":body.split('\n#%s'%(providerchar))[0],
 			}
 		)
-		sections[-1]['hash'] = hashlib.md5(lasthash+json.dumps(sections[-1]))).hexdigest()
+		sections[-1]['hash'] = hashlib.md5(lasthash+json.dumps(sections[-1])).hexdigest()
 		lasthash = sections[-1]['hash']
 		lasthash = sections[-1]['provider']
 	sections.remove(sections[0]) ## Remove blank entry
