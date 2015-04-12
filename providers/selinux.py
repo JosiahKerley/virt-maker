a06b61f86e3d9b7e1a11e559933c31e2c668fff4
Josiah Kerley <josiahkerley@gmail.com>
2015-04-12 03:06:27 -0600

    ``

---
+++ providers/selinux.py
@@ -0,0 +1,12 @@
+import os
+def provider(body,hash,args,verbose,image,settings):
+	sed = "sed -i '/^SELINUX=.*/s/^SELINUX=.*/SELINUX=%s/g' /etc/selinux/config"
+	if   'disabl' in args.lower(): command = sed%('disabled')
+	elif 'enforc' in args.lower(): command = sed%('enforcing')
+	elif 'permis' in args.lower(): command = sed%('permissive')
+	else: return('Unknown argument')
+	if verbose:
+		cmd = 'virt-customize --run-command "%s" -a %s'%(args,hash)
+	else:
+		cmd = 'virt-customize -q --run-command "%s" -a %s >/dev/null 2>&1'%(args,hash)
+	return(os.system(cmd))
--- providers/virt-customize.py
+++
@@ -1,9 +0,0 @@
-import os
-def provider(body,hash,args,verbose,image,settings):
-	cmd = 'virt-customize'
-	if verbose:
-		cmd = '%s %s'%(cmd,args)
-		print(cmd)
-	else:
-		cmd = '%s --quite %s >/dev/null 2>&1'%(cmd,args)
-	return(os.system(cmd))
