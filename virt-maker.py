5bdcb52d1d54e7fdd9bd23da0eb0cd67fcdfcc0c
Josiah Kerley <josiahkerley@gmail.com>
2015-04-12 01:41:33 -0600

    ``

--- virt-maker.py
+++ virt-maker.py
@@ -46,8 +46,8 @@ def dsl2opt(text,providerchar='@'):
 
 ## Parses DSL Statements
 def dsl2dict(text,options=False,mutatestr='<[%s]>', providerchar='@'):
-	text = providerchar.join(text.split(providerchar)[1:])
 	text = text.replace('\n%s'%(providerchar),'\n\n%s'%(providerchar))
+	text = providerchar.join(text.split(providerchar)[1:])
 	if options:
 		for i in options:
 			key = mutatestr%(i.split('=')[0])
