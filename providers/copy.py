8910b8352dfbdd3076ff67e0550fff0bd0eedd62
Josiah Kerley <josiahkerley@gmail.com>
2015-04-13 06:25:21 -0600

    Added copy provider

---
+++ providers/copy.py
@@ -0,0 +1,19 @@
+import os
+import uuid
+def provider(body,hash,args,verbose,image,settings):
+
+	## Parse args
+	if args == "":
+		args = False
+	else:
+		args = args.split(' ')
+	src = args[0]
+	try: dest = args[1]
+	except: dest = src
+	dir = os.path.dirname(dest)
+	if verbose:
+		cmd = 'virt-customize --mkdir "%s" --upload "%s":"%s" -a %s'%(dir,src,dest,hash)
+		print cmd
+	else:
+		cmd = 'virt-customize -q --mkdir "%s" --upload "%s":"%s" -a %s >/dev/null 2>&1'%(dir,src,dest,hash)
+	return(os.system(cmd))
