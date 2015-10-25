445857a7af4b4476ca4051a15e6f97caa94a5c1b
Josiah Kerley <josiahkerley@gmail.com>
2015-10-25 12:56:53 -0600

    Added flock until proper locking is introduced

--- setup.py
+++ setup.py
@@ -6,7 +6,8 @@ from setuptools import setup
 
 ## Globals
 here = os.path.dirname(os.path.realpath(__file__))
-install_file = '/usr/local/bin/virt-maker'
+install_file = '/usr/local/bin/virt-maker.py'
+cli_file = '/usr/local/bin/virt-maker'
 install_dirs = [
   '/usr/local/bin',
   '/var/lib/virt-maker',
@@ -14,14 +15,17 @@ install_dirs = [
 ]
 
 
-## Globals
-here = os.path.dirname(os.path.realpath(__file__))
-install_file = '/usr/local/bin/virt-maker'
-install_dirs = [
-  '/usr/local/bin',
-  '/var/lib/virt-maker',
-  '/var/lib/virt-maker/providers',
-]
+## CLI
+script = '''
+#!/bin/bash
+function error(){
+  echo "Virt-Maker locked"
+  ps -ef | grep flock | grep virt-maker
+}
+flock -n '/tmp/.virt-maker.lock' -c "%s $*" || error
+'''%(install_file)
+
+
 
 
 ## Main Setup
@@ -45,6 +49,9 @@ if 'install' in sys.argv:
   with open(install_file,'w') as f:
     f.write(file_content)
   os.system('chmod +x "%s"'%(install_file))
+  with open(cli_file,'w') as f:
+    f.write(script)
+  os.system('chmod +x "%s"'%(cli_file))
   for provider in os.listdir(here+'/providers'):
     if provider.endswith('.py'):
       with open(here+'/providers/'+provider,'r') as f:
